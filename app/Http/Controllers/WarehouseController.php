36d57c25639c3556b57e3fd0e7144573389e957b
mrdjarade <rademrdja.mrdja@gmail.com>
2022-06-19 11:04:40 +0200

    Kreiran kontroler za rad sa skladistem

---
+++ app/Http/Controllers/WarehouseController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StorageUnit;
+use App\Models\Warehouse;
+use Illuminate\Http\Request;
+
+class WarehouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Warehouse::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $warehouse = Warehouse::create($request->all());
+        return response()->json($warehouse);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Warehouse $warehouse)
+    {
+        return response()->json($warehouse);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Warehouse $warehouse)
+    {
+        $warehouse->update($request->all());
+        return response()->json($warehouse);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Warehouse $warehouse)
+    {
+        $warehouse->delete();
+        return response()->noContent();
+    }
+
+    public function addProduct($id, Request $request)
+    {
+        $storageUnit = StorageUnit::create([
+            "count" => $request->count,
+            "product_id" => $request->product_id,
+            'warehouse_id' => $id
+        ]);
+        return response()->json(Warehouse::find($id));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('warehouses', WarehouseController::class);
+    Route::post('warehouese/{id}/product', WarehouseController::class, 'addProduct');
 });
